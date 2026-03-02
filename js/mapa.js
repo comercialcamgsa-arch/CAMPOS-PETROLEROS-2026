beaa482982eba33a5c494902159232d8b84091af
comercialcamgsa-arch <comercialcamgsa@gmail.com>
2026-03-02 10:27:25 -0600

    Refactor initMap function to use window scope

--- js/mapa.js
+++ js/mapa.js
@@ -1,14 +1,18 @@
-function initMap() {
+window.initMap = function () {
 
-  const ubicacion = { lat: 17.9892, lng: -92.9475 };
+  const ubicacion = { lat: 17.9892, lng: -92.9475 }; // Cambia coordenadas si quieres
 
-  const mapa = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
-    center: ubicacion,
-  });
+  const mapa = new google.maps.Map(
+    document.getElementById("map"),
+    {
+      zoom: 14,
+      center: ubicacion,
+    }
+  );
 
   new google.maps.Marker({
     position: ubicacion,
     map: mapa,
   });
-}
+
+};
