93908dbe9c80e4aaf17ef8d5f603225fe85de7ab
comercialcamgsa-arch <comercialcamgsa@gmail.com>
2026-03-02 11:16:23 -0600

    Add initial data for oil wells in pozos.js

---
+++ pozos.js
@@ -0,0 +1,59 @@
+const pozos = [
+  // PEMEX
+  {
+    nombre: "Ku-Maloob-Zaap 101",
+    operador: "Pemex",
+    campo: "Ku-Maloob-Zaap",
+    estado: "Campeche",
+    lat: 19.396,
+    lng: -92.038
+  },
+  {
+    nombre: "Cantarell 245",
+    operador: "Pemex",
+    campo: "Cantarell",
+    estado: "Campeche",
+    lat: 19.734,
+    lng: -92.210
+  },
+
+  // ENI
+  {
+    nombre: "Miztón-2",
+    operador: "Eni",
+    campo: "Amoca-Miztón-Tecoalli",
+    estado: "Campeche",
+    lat: 19.102,
+    lng: -92.012
+  },
+
+  // FIELDWOOD
+  {
+    nombre: "Ichalkil-3",
+    operador: "Fieldwood",
+    campo: "Ichalkil-Pokoch",
+    estado: "Campeche",
+    lat: 19.221,
+    lng: -91.876
+  },
+
+  // WOODSIDE
+  {
+    nombre: "Trion-1",
+    operador: "Woodside",
+    campo: "Trion",
+    estado: "Golfo de México",
+    lat: 25.123,
+    lng: -94.321
+  },
+
+  // HOKCHI
+  {
+    nombre: "Hokchi-7",
+    operador: "Hokchi",
+    campo: "Hokchi",
+    estado: "Campeche",
+    lat: 19.812,
+    lng: -91.543
+  }
+];
